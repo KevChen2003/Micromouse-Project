1f86fcf1fd6790689dff34d4644e990c8cd29bf8
DO-NOT-TOUCH <z5360742@ad.unsw.edu.au>
2024-08-14 17:29:15 +1000

    merge conflcits

--- testFile/robot.hpp
+++ testFile/robot.hpp
@@ -28,7 +28,7 @@
 
 #define SCREEN_WIDTH 128 // OLED display width in pixels
 #define SCREEN_HEIGHT 64 // OLED display height in pixels
-#define OLED_RESET -1 // Reset pin number (or -1 if sharing Arduino reset pin)
+#define OLED_RESET -1    // Reset pin number (or -1 if sharing Arduino reset pin)
 
 const float WALL_DETECTION = 80;
 // Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); // Creates an OLED display
@@ -36,87 +36,42 @@ MPU6050 mpu(Wire); // Creates an IMU
 
 // Moving Average Filter for Yaw
 unsigned long timer = 0;
-const int numReadings = 5; // Number of readings to average
+const int numReadings = 5;      // Number of readings to average
 float yawReadings[numReadings]; // Array to store yaw readings
-int index = 0; // Index to track position in the array
+int index = 0;                  // Index to track position in the array
 
 mtrn3100::DualEncoder encoder(EN_1_A, EN_1_B, EN_2_A, EN_2_B);
-mtrn3100::EncoderOdometry encoder_odometry(16,90); // Wheel radius & axial length
-mtrn3100::Motor R_Motor(9, 10); // correct
+mtrn3100::EncoderOdometry encoder_odometry(16, 90); // Wheel radius & axial length
+mtrn3100::Motor R_Motor(9, 10);                     // correct
 mtrn3100::Motor L_Motor(11, 12);
 mtrn3100::Lidar lidar;
 mtrn3100::IMU imu;
 // mtrn3100::OLED oled;
 
-// tune pids
-
-// tuned to 1 cell, a bit slow
-// left: 9,0,0.0012
-// right 8,0, 0
-
-// tuning for multiple cells
-// left: 9,0,0
-// right: 8,0,0
-
-
-// mtrn3100::PIDController l_forward_pid(9,0,0);
-// mtrn3100::PIDController r_forward_pid(8,0,0);
-
-// mtrn3100::PIDController side_lidar_pid(0.5,0.1,0);
-// mtrn3100::PIDController front_lidar_pid(0.5,0,0);
-
-// mtrn3100::PIDController l_forward_pid(49.7654,0.0512,2.9);
-// mtrn3100::PIDController r_forward_pid(48.7654,0.295,3)7
-// mtrn3100::PIDController l_forward_pid(49.55,0.07,4);
-// mtrn3100::PIDController r_forward_pid(48.775,0.24577,3.73); // works
-
 // CHAINING MVOEMENTS
 // mtrn3100::PIDController l_forward_pid(49,0.25,6.5); // 49,0,3.5
 // mtrn3100::PIDController r_forward_pid(55,0,7); // 58,0,3.5
 
-// mtrn3100::PIDController l_forward_pid(30.15,0,4.9); // 49,0,3.5 // 
-// mtrn3100::PIDController r_forward_pid(36,2.7069,11); // 58,0,3.5 // 
-
-mtrn3100::PIDController l_forward_pid(33,0,4.5); // 33,0.1,4.5
-mtrn3100::PIDController r_forward_pid(36,0,3.5); // 36,0.5,3.5 
-
-mtrn3100::PIDController side_lidar_pid(1,0,0);
+mtrn3100::PIDController l_forward_pid(10.7, 0.2, 0.02); // WORKS ONE CELL 10.6, 0.1, 0.05 //  11.6, 0.1, 0.1// trying longer distance 3.5, 0, 0.4
+mtrn3100::PIDController r_forward_pid(11.6, 0.2, 0.027);  // WORKS ONE CELL 11.6, 0.2, 0.0 12.6, 0.2, 0.0// // trying longer distance 3.9, 0.15, 0.2
+mtrn3100::PIDController side_lidar_pid(2, 0, 0.05);
+mtrn3100::PIDController mpu_forward_pid(2, 0, 0);
 // mtrn3100::PIDController front_lidar_pid(1,0,0);
 
-// ONLY READING LIDARS 
+// ONLY READING LIDARS
 // mtrn3100::PIDController side_lidar_pid(0.55,0.1,0.2);
-mtrn3100::PIDController front_lidar_pid(0.5,0,0);
-
-// 20, 0, 3
-// 17.5 50 3.3 (no error factor)
-// 4.5, 0, 1 (error factor of 5)
-
-// will slowly go towards 0 error, although very slow
-// 4.175, 0.65, 1.0011
-
-// faster, but overshoots by a fair bit
-// 4.175, 0.65, 1.0015
-
-// 12.1 4.315 0.61 (error factor 10)
+mtrn3100::PIDController front_lidar_pid(0.5, 0, 0);
 
 mtrn3100::PIDController encoder_odometry_h_pid(4.2, 0, 1);
-// NOOOO
-// mtrn3100::PIDController mpu_pid_right(11.1685,0.043,0.195); // previous one
-// mtrn3100::PIDController mpu_pid_right(11.1685,0.04,0.195); // best one for turning right
 
-// mtrn3100::PIDController mpu_pid_left(11.16085,0.0417,0.211); // sliiiiighty off but it works
-// mtrn3100::PIDController mpu_pid_right(11.152,0.0444,0.207);
-
-// NOOOOOOO
-mtrn3100::PIDController mpu_pid_right(11.154,0.055,0.207);
-
-mtrn3100::PIDController mpu_pid_left(11.155,0.044,0.207);
-
+mtrn3100::PIDController mpu_pid_right(11.154, 0.055, 0.207);
+mtrn3100::PIDController mpu_pid_left(11.155, 0.044, 0.207);
 
 // Function declarations for Movement
 void moveForward(int cell_count);
 int driveMotors(float l_target, float r_target);
 void straight();
+// void autonomous();
 
 float l_forward_signal = 0;
 float r_forward_signal = 0;
@@ -129,9 +84,8 @@ float r_drive_signal = 0;
 float drive_signal = 0;
 float right_signal = 0;
 float left_signal = 0;
-float bias = 0;
-float adjusted_Rpwm = 0;
 
+unsigned long prevMillis = 0;
 // const float circumference = 2*PI*0.016;
 // const float distance_per_count = circumference/700;
 
